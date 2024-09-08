238d737c0034a905eea2e61124a825e36d86bb65
gusDev <joaogustavolima084@gmail.com>
2024-09-08 15:51:02 -0300

    docs: atualização nos arquivos headers hash e produto

--- include/produto.h
+++ include/produto.h
@@ -5,4 +5,17 @@
 
 typedef struct produto Produto;
 
+int altura(Produto *produto);
+int max(int a, int b);
+int getBalanceamento(Produto *produto);
+Produto* rotacaoDireita(Produto *y);
+Produto* rotacaoEsquerda(Produto *x);
+Produto* buscaMenorProduto(Produto *produto);
+Produto* criaProduto(char *nomeProduto, float preco, int quantidade);
+Produto* insereProdutoAVL(Produto *raiz, Produto *novoProduto);
+void imprimeProdutos(Produto *raiz);
+Produto* buscaProdutoAVL(Produto *raiz, char *nome);
+Produto* removeProdutoAVL(Produto *raiz, char *nome);
+void liberaProdutosAVL(Produto *raiz);
+
 #endif // PACIENTE_H
